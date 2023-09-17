b57c3c90961e44cdb61c7839a4be30ed3e375296
will <jaskamiin@gmail.com>
2023-09-17 15:49:25 -0400

    Add project files.

---
+++ Source/KainiAstra/KainiAstraGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "KainiAstraGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KAINIASTRA_API AKainiAstraGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
